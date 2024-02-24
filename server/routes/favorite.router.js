0caa77d7360b135c997f1390d2e0a3335be293e4
Ying Her <yher.prime@gmail.com>
2024-02-24 09:28:43 -0600

    making api call to giphy for search and mapping results for FE

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const axios = require('axios');
 const pool = require('../modules/pool');
 const router = express.Router();
 const API_KEY = process.env.API_KEY;
 
 //testing process.env
 console.log('API_KEY:', API_KEY);
+
+const BASE_URL_GIPHY = 'api.giphy.com/v1/gifs/search';
 
 // return all favorite images
 router.get('/', (req, res) => {
@@ -23,20 +26,43 @@ router.get('/', (req, res) => {
 
 // add a new favorite
 router.post('/', (req, res) => {
-  const favorite = req.body;
-  const sqlText = `INSERT INTO "favorites" ("giphy_image_url", "category_id")
-  VALUES ($1, $2)`;
-  pool
-    .query(sqlText, [favorite.giphy_image_url, favorite.category_id])
-    .then((result) => {
-      console.log(`Added favorite to the database`, favorite);
-      res.sendStatus(201);
+  const { searchTerm } = req.body;
+  //? query string comes after the ? key=value&keyTwo=valueTwo = api_key& q represent searchTerm
+  axios
+    .get(
+      `https://${BASE_URL_GIPHY}?api_key=${API_KEY}&q=${searchTerm}&limit=10`
+    )
+    .then((giphyResponse) => {
+      console.log('SUCCESS', giphyResponse.data);
+      const simpleResults = giphyResponse.data.data.map((imageData) => {
+        return {
+          // this is how we like our structure to look like in the front end --> test Postman to make sure this is the structure it's rendering out
+          id: imageData.id,
+          url: imageData.images.original.url,
+          alt: imageData.title,
+        };
+      });
+      res.send(simpleResults);
     })
     .catch((error) => {
-      console.log(`Error making database query ${sqlText}`, error);
+      console.log('Error posting giphy search to database', error);
       res.sendStatus(500);
     });
 });
+
+//   const sqlText = `INSERT INTO "favorites" ("giphy_image_url", "category_id")
+//   VALUES ($1, $2)`;
+//   pool
+//     .query(sqlText, [favorite.giphy_image_url, favorite.category_id])
+//     .then((result) => {
+//       console.log(`Added favorite to the database`, favorite);
+//       res.sendStatus(201);
+//     })
+//     .catch((error) => {
+//       console.log(`Error making database query ${sqlText}`, error);
+//       res.sendStatus(500);
+//     });
+// });
 
 // update a favorite's associated category
 router.put('/:id', (req, res) => {
