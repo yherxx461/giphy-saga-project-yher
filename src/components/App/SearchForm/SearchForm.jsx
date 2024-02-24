bfcc66dd27779cbadd98e3b1608c619d2c11e588
Ying Her <yher.prime@gmail.com>
2024-02-24 00:33:50 -0600

    SearchForm created

---
+++ src/components/App/SearchForm/SearchForm.jsx
@@ -0,0 +1,14 @@
+function SearchForm() {
+  return (
+    <form>
+      <input
+        id="name"
+        placeholder="Element name"
+        type="text"
+        // value & onChange (event) function
+      />
+    </form>
+  );
+}
+
+export default SearchForm;
